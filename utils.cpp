28835698a9991978cd6eb494a9cd826fdae74640
Javier <javier_gn@hotmail.cl>
2014-10-17 16:01:24 -0300

    Improving bit interleaving algorithm

--- utils.cpp
+++ utils.cpp
@@ -81,9 +81,39 @@ void Utils::PrintBinary(unsigned int decimalNumber, bool bNormal)
 
 void Utils::CreateQuadCode(int x, int y, BitmapWrapper* bitmapWrapper, int quadLength)
 {
+  //unsigned short x1 = x;
+  //unsigned short y1 = y;
+  
+  uint z = MortonTable256[x >> 8]   << 17 | 
+           MortonTable256[y >> 8]   << 16 |
+           MortonTable256[x & 0xFF] <<  1 | 
+           MortonTable256[y & 0xFF];
+  
   bitmapWrapper->len = quadLength;//(floor(log2(x)) + 1) + (floor(log2(y) + 1));
   bitmapWrapper->bitmap = new uint[uint_len(bitmapWrapper->len, 1)];
   
+  
+  for(int i = quadLength - 1; i >= 0; i--)
+  {
+    //bitset(bitmapWrapper->bitmap, i);
+      (z & 1) ? bitset(bitmapWrapper->bitmap, i) : bitclean(bitmapWrapper->bitmap, i);
+      z >>= 1;
+  }
+  
+  /*
+  set_field(bitmapWrapper->bitmap, bitmapWrapper->len, 0, z);
+  for(int i = 0; i < quadLength; i++)
+  {
+      cout << bitget(bitmapWrapper->bitmap, i);
+  }
+  cout << endl;
+  
+  //set_field(bitmapWrapper->bitmap, bitmapWrapper->len, 0, z);
+  */
+  
+  //======================
+  /*
+  int tmpL = quadLength;
   quadLength--;
   
   //cout << quadLength << endl;
@@ -110,6 +140,14 @@ void Utils::CreateQuadCode(int x, int y, BitmapWrapper* bitmapWrapper, int quadL
     bitclean(bitmapWrapper->bitmap, quadLength);
     quadLength--;
   }
-  
+  */
+  /*
+  for(int i = 0; i < tmpL; i++)
+  {
+      cout << bitget(bitmapWrapper->bitmap, i);
+  }
+  cout << endl;
+  */
   //bitset(bitmapWrapper->bitmap, 0);
+  
 }
